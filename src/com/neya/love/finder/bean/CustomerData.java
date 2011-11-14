351c8a6279d50b3bf85f189f0aa9ecb7b9f7c8c6
Nikolay Yanev <yanev93@gmail.com>
2011-11-14 12:24:17 +0200

    - Added connection with mysql db - Removed Tet file

--- src/com/neya/love/finder/bean/CustomerData.java
+++ src/com/neya/love/finder/bean/CustomerData.java
@@ -4,11 +4,6 @@
  */
 package com.neya.love.finder.bean;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author nyanev
  * 
--- src/com/neya/love/finder/bean/Test.java
+++
@@ -1,5 +0,0 @@
-package com.neya.love.finder.bean;
-
-public class Test {
-
-}
